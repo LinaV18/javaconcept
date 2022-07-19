32f1bc672855a2c0ae02afb209144748d913b4c3
Lina Valand <linavaland@hotmail.co.uk>
2022-07-19 22:52:41 +0100

    myfirstprogram

---
+++ myfirstprogram.java
@@ -0,0 +1,9 @@
+package javaconcept;
+
+public class myfirstprogram {
+
+    public static void main(String[] args) {
+        System.out.println("My First class");
+        System.out.println("lina valand");
+    }
+}
